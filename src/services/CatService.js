12dbd9db368b7a9ab56123877676893820f06cf8
Daniel <daniel.hammami@yahoo.com>
2024-04-08 13:59:13 +0200

    Implémentation de CatService pour charger les données des chats depuis l'API

--- src/services/CatService.js
+++ src/services/CatService.js
@@ -1,5 +1,15 @@
-const CatService = () => {
-  return <div>CatService</div>;
+import axios from "axios";
+
+const CATS_API_URL = "https://data.latelier.co/cats.json";
+
+const getCats = async () => {
+  try {
+    const response = await axios.get(CATS_API_URL);
+    return response.data.images;
+  } catch (error) {
+    console.error("Erreur lors du chargement des données des chats:", error);
+    throw error;
+  }
 };
 
-export default CatService;
+export default getCats;
